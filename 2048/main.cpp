92710f26fa122aefa705dbf8db683b76de3a714f
Razvan Serban <razvan_tankian@yahoo.com>
2018-01-27 00:30:32 +0200

    First Text

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -11,10 +11,10 @@ int main()
 	sf::VertexArray quads(sf::Quads, 4);
 
 	// define the position of the triangle's points
-	quads[0].position = sf::Vector2f(10, 10);
-	quads[1].position = sf::Vector2f(100, 10);
-	quads[2].position = sf::Vector2f(100, 100);
-	quads[3].position = sf::Vector2f(10, 100);
+	quads[0].position = sf::Vector2f(10, 80);
+	quads[1].position = sf::Vector2f(100, 80);
+	quads[2].position = sf::Vector2f(100, 170);
+	quads[3].position = sf::Vector2f(10, 170);
 
 	sf::Color tailColor = sf::Color(255, 255, 122);
 
@@ -24,7 +24,20 @@ int main()
 	Tile myTile2 = Tile::Tile();
 	std::printf("value = %d\nposition (%d, %d)\n", myTile2.getValue(), myTile2.getPosition().x, myTile2.getPosition().y);
 
-	//test
+	//2048 text
+	sf::Text text;
+	sf::Font font;
+	
+	// Load it from a file
+	if (!font.loadFromFile("arial.ttf"))
+	{
+		
+	}
+	text.setFont(font);
+	text.setString("2048");
+	text.setCharacterSize(60);
+	text.setFillColor(sf::Color(0,0,122));
+	text.setStyle(sf::Text::Bold);
 
 	// define the color of the triangle's points
 	for (int i = 0; i < 4; i++)
@@ -41,6 +54,7 @@ int main()
 
 		window.clear(sf::Color(0, 122, 255, 255));
 		window.draw(quads);
+		window.draw(text);
 		window.display();
 	}
 
